03ae2b1141a52e104fb09bfad7a1753056ea43b4
wǒ_is神仙 <i@mrzhang.me>
2014-04-26 23:48:09 +0800

    cache support

--- lib/base.sass/json-parse.rb
+++ lib/base.sass/json-parse.rb
@@ -1,8 +1,31 @@
 module Sass::Script::Functions
 
+  $cached_files = {}
+
   def json_parse(path)
     assert_type path, :String
-    ruby_to_sass(CanIUse.instance.load(path.value))
+    path = File.expand_path(path.value)
+
+    if $cached_files.key? path
+      puts "Read data from cache: #{path}"
+      $cached_files[path]
+    else
+      $cached_files[path] = ruby_to_sass(json_load(path))
+    end
+  end
+
+
+  protected
+
+  def json_load(path)
+    path = File.expand_path(path)
+
+    if File.readable? path
+      puts "Load JSON file: #{path}"
+      JSON.load(File.read(path).to_s.gsub(/(\\r|\\n)/, ''))
+    else
+      raise Sass::SyntaxError, "JSON file not found or cannot be read: #{path}"
+    end
   end
 
 end
