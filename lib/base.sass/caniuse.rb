20f9629ab35ede0eadf9dcca6ec77708e4f1a68a
wǒ_is神仙 <i@mrzhang.me>
2014-04-27 00:05:05 +0800

    update caniuse class

--- lib/base.sass/caniuse.rb
+++ lib/base.sass/caniuse.rb
@@ -1,33 +1,22 @@
-require 'json'
 require 'singleton'
 
 class CanIUse
   include Singleton
+  include Sass::Script::Functions
 
   DATA_DIR = File.join(File.dirname(__FILE__), '..', '..', 'data')
 
   def initialize
-    @caniuse_browsers = load(File.join(DATA_DIR, 'browsers.json'))
-    @caniuse_supports = load(File.join(DATA_DIR, 'supports.json'))
+    @caniuse_browsers = json_load(File.join(DATA_DIR, 'browsers.json'))
+    @caniuse_supports = json_load(File.join(DATA_DIR, 'supports.json'))
   end
 
-  def browsers(prefix = nil)
-    if prefix
-      @caniuse_browsers.select { |k, v| v['prefix'] == prefix }.keys
-    else
-      @caniuse_browsers.keys
-    end
+  def browsers_data
+    @caniuse_browsers
   end
 
-  def load(path)
-    path = File.expand_path(path)
-
-    if File.readable? path
-      puts "Loading JSON file: #{path}"
-      JSON.load(File.read(path))
-    else
-      raise Sass::SyntaxError, "JSON file not found or cannot be read: #{path}"
-    end
+  def supports_data
+    @caniuse_browsers
   end
 
 end
