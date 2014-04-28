69024c9acf937edc23c3d4ebe45277789b3cebc2
wǒ_is神仙 <i@mrzhang.me>
2014-04-28 13:31:14 +0800

    add sass_to_ruby

---
+++ lib/base.sass/sass-to-ruby.rb
@@ -0,0 +1,25 @@
+module Sass::Script::Functions
+
+  protected
+
+  def sass_to_ruby(obj)
+    return to_ruby_hash(obj) if obj.is_a? Sass::Script::Value::Map
+    return to_ruby_array(obj) if obj.is_a? Sass::Script::Value::List
+    return obj.inspect if obj.is_a? Sass::Script::Value::Color
+    obj.value
+  end
+
+  def to_ruby_hash(sass_map)
+    sass_map.value.inject({}) do |memo, (k, v)|
+      memo[k.to_s] = sass_to_ruby(v)
+      memo
+    end
+  end
+
+  def to_ruby_array(sass_list)
+    sass_list.value.map do |item|
+      sass_to_ruby(item)
+    end
+  end
+
+end
