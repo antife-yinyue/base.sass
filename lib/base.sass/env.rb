fd9d29c80c9857a9c9c5e7b70560bba526f74ca9
wǒ_is神仙 <i@mrzhang.me>
2014-04-30 17:48:52 +0800

    env() supports hyphen-style param

--- lib/base.sass/env.rb
+++ lib/base.sass/env.rb
@@ -2,7 +2,7 @@ module Sass::Script::Functions
 
   def env(name)
     assert_type name, :String
-    ruby_to_sass(ENV[name.value.upcase])
+    ruby_to_sass(ENV[name.value.gsub('-', '_').upcase])
   end
 
 end
