8b825df633de9e6fda2f81decc38cbf99686f4d2
wǒ_is神仙 <i@mrzhang.me>
2014-04-30 22:08:08 +0800

    update

--- lib/base.sass.rb
+++ lib/base.sass.rb
@@ -1,3 +1,10 @@
+root_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+load_path = File.join(root_path, 'stylesheets')
+
+ENV['SASS_ENV'] ||= 'development'
+ENV['SASS_PATH'] = [ENV['SASS_PATH'], load_path].compact.join(File::PATH_SEPARATOR)
+ENV['WHICH_BASE.SASS'] = root_path + File::SEPARATOR
+
 require 'base.sass/caniuse'
 require 'base.sass/ruby-to-sass'
 require 'base.sass/sass-to-ruby'
@@ -8,16 +15,3 @@ require 'base.sass/env'
 require 'base.sass/strftime'
 require 'base.sass/url'
 require 'base.sass/selector'
-
-
-root_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-load_path = File.join(root_path, 'stylesheets')
-
-if ENV.key? 'SASS_PATH'
-  ENV['SASS_PATH'] += File::PATH_SEPARATOR + load_path
-else
-  ENV['SASS_PATH'] = load_path
-end
-
-ENV['SASS_ENV'] ||= 'development'
-ENV['WHICH_BASE.SASS'] = root_path + File::SEPARATOR
