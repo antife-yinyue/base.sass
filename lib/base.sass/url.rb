04bb9585ae1ee04801e07cadb73ae36b4e138628
wǒ_is神仙 <i@mrzhang.me>
2014-05-03 00:45:01 +0800

    url helpers support list-style argument

--- lib/base.sass/url.rb
+++ lib/base.sass/url.rb
@@ -1,6 +1,6 @@
 module Sass::Script::Functions
 
-  PATH_PARSER = /^(.*)(\.\w+)(\??[^#]*)(#?.*)$/
+  PATH_REGEX = /^(.*)(\.\w+)(\??[^#]*)(#?.*)$/
 
   FONT_TYPES = {
     eot: 'embedded-opentype',
@@ -20,70 +20,85 @@ module Sass::Script::Functions
     svg: 'image/svg+xml'
   }
 
-  def font_url(path, cache_buster = bool(true))
-    assert_type path, :String
+  def font_url(paths, cache_buster = bool(true))
+    ts = timestamp(cache_buster)
 
-    ext, query, anchor = ''
-    PATH_PARSER.match(path.value) do
-      path, ext, query, anchor = $1 + $2, $2[1..-1].downcase.to_sym, $3, $4
+    urls = resolve_paths(paths).map do |path|
+      if path =~ PATH_REGEX
+        path, ext, query, anchor = $1 + $2, $2[1..-1].downcase.to_sym, $3, $4
+        query += sign(query) + ts unless ts.nil?
+
+        raise Sass::SyntaxError, "Could not determine font type for #{path}" unless FONT_TYPES.key? ext
+
+        identifier("url(#{path}#{query}#{anchor}) format('#{FONT_TYPES[ext]}')")
+      else
+        quoted_string(path)
+      end
     end
 
-    raise Sass::SyntaxError, "Could not determine font type for #{path}" unless FONT_TYPES.key? ext
-
-    query = join_query(query, cache_buster) if cache_buster.to_bool
-
-    identifier("url(#{path}#{query}#{anchor}) format('#{FONT_TYPES[ext]}')")
+    list(urls, :comma)
   end
 
-  def image_url(path, cache_buster = bool(true))
-    assert_type path, :String
+  def image_url(paths, cache_buster = bool(true))
+    ts = timestamp(cache_buster)
 
-    query, anchor = ''
-    PATH_PARSER.match(path.value) do
-      path, query, anchor = $1 + $2, $3, $4
+    urls = resolve_paths(paths).map do |path|
+      if path =~ PATH_REGEX
+        path, query, anchor = $1 + $2, $3, $4
+        query += sign(query) + ts unless ts.nil?
+
+        identifier("url(#{path}#{query}#{anchor})")
+      else
+        quoted_string(path)
+      end
     end
 
-    query = join_query(query, cache_buster) if cache_buster.to_bool
-
-    identifier("url(#{path}#{query}#{anchor})")
+    list(urls, :comma)
   end
 
-  def data_url(path)
-    assert_type path, :String
+  def data_url(paths)
+    urls = resolve_paths(paths).map do |path|
+      if path =~ PATH_REGEX
+        path, ext = $1 + $2, $2[1..-1].downcase.to_sym
 
-    ext = ''
-    PATH_PARSER.match(File.expand_path(path.value)) do
-      path, ext = $1 + $2, $2[1..-1].downcase.to_sym
+        raise Sass::SyntaxError, "Could not determine mime type for #{path}" unless MIME_TYPES.key? ext
+
+        data = [read_file(File.expand_path(path))].flatten.pack('m').gsub(/\s/, '')
+        identifier("url(data:#{MIME_TYPES[ext]};base64,#{data})")
+      else
+        quoted_string(path)
+      end
     end
 
-    raise Sass::SyntaxError, "Could not determine mime type for #{path}" unless MIME_TYPES.key? ext
-
-    data = [read_file(path)].flatten.pack('m').gsub(/\s/, '')
-    identifier("url(data:#{MIME_TYPES[ext]};base64,#{data})")
+    list(urls, :comma)
   end
 
 
   private
 
-  def join_query(query, cache_buster)
-    query = query.to_s
+  def timestamp(cache_buster)
+    return nil unless cache_buster.to_bool
 
-    query << case query.size
-      when 0
-        '?'
-      when 1
-        ''
-      else
-        '&'
-      end
-
-    query << if cache_buster.is_a? Sass::Script::Value::String
+    if cache_buster.is_a? Sass::Script::Value::String
       cache_buster.value
     else
       strftime.value
     end
+  end
 
-    query
+  def sign(query)
+    case query.size
+    when 0
+      '?'
+    when 1
+      ''
+    else
+      '&'
+    end
+  end
+
+  def resolve_paths(paths)
+    [paths].map { |path| sass_to_ruby(path) }.flatten
   end
 
 end
