be3720dfa112c93d313a8beb3cd1798c33c982b5
wǒ_is神仙 <i@mrzhang.me>
2014-05-05 20:57:00 +0800

    add var() and config()

---
+++ lib/base.sass/var.rb
@@ -0,0 +1,21 @@
+module Sass::Script::Functions
+
+  # Get the value of the specified global variable
+  def var(name)
+    assert_type name, :String
+    environment.global_env.var(name.value) || null()
+  end
+
+  # Get the configuration settings
+  def config(name)
+    namespace = 'sass-config'
+    config = environment.global_env.var(namespace)
+
+    raise Sass::SyntaxError, "The global variable `$#{namespace}` not found" if config.nil?
+    raise Sass::SyntaxError, "The global variable `$#{namespace}` is not a map" unless config.is_a? Sass::Script::Value::Map
+    assert_type name, :String
+
+    map_get(config, name)
+  end
+
+end
