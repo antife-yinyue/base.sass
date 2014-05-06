a525b82934a15b2f1f70927d5fcac554b166721a
wǒ_is神仙 <i@mrzhang.me>
2014-05-06 23:07:41 +0800

    cleanup

--- lib/base.sass.rb
+++ lib/base.sass.rb
@@ -1,5 +1,4 @@
-root_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-load_path = File.join(root_path, 'stylesheets')
+load_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'stylesheets'))
 
 ENV['SASS_PATH'] = [ENV['SASS_PATH'], load_path].compact.join(File::PATH_SEPARATOR)
 ENV['SASS_ENV'] ||= 'development'
