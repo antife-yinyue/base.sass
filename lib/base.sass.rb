94aaf36f0696dde8f463b0d8bab65a002dcb4368
wǒ_is神仙 <i@mrzhang.me>
2014-05-23 11:31:10 +0800

    register as a Compass extension

--- lib/base.sass.rb
+++ lib/base.sass.rb
@@ -1,7 +1,15 @@
 load_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'stylesheets'))
 
+# Register on the Sass path via the environment.
 ENV['SASS_PATH'] = [ENV['SASS_PATH'], load_path].compact.join(File::PATH_SEPARATOR)
 ENV['SASS_ENV'] ||= 'development'
+
+# Register as a Compass extension.
+begin
+  require 'compass'
+  Compass::Frameworks.register('base.sass', stylesheets_directory: load_path)
+rescue LoadError
+end
 
 require 'base.sass/caniuse'
 require 'base.sass/env'
